b31665075773e74ec877f445b2d17ec03829bd82
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-02-17 01:51:15 +0000

    “basic_math_updated.py

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Xiaohan
+# Date: Feb 14, 2025
+# this program does basic math.
+
+
+def main():
+    print("5-3={}".format(5 - 3))
+    print("2+10={}".format(2 + 10))
+    print("8*5={}".format(8 * 5))
+    print("4/3={}".format(4 / 3))
+    print("1/6={}".format(1 / 6))
+    print("2+5*7={}".format(2 + 5 * 7))
+    print("2^3={}".format(2 ^ 3))  # bitwise operators
+    print("2**4={}".format(2**4))
+    print("16**5={}".format(16**5))
+
+
+if __name__ == "__main__":
+    main()
